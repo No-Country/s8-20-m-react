c9ff460e482422704629c49051aa3c4879a4cfef
Renzo Lacovara <rnlacovara@gmail.com>
2023-05-15 20:51:21 -0300

    feat: api backend

--- pages/api/routers/products.router.js
+++ pages/api/routers/products.router.js
@@ -1,19 +1,19 @@
-import { Router } from 'express'
-import { passportCall } from '../../config/utility/utils.js'
+import { Router } from 'express';
+import { passportCall } from '../config/utility/utils.js';
 import {
   deleteProduct,
   getProductById,
   getProducts,
   saveProduct,
   updateProduct,
-} from '../controllers/products.controller.js'
+} from '../controllers/products.controller.js';
 
-const router = Router()
+const router = Router();
 
-router.get('/', passportCall('jwt'), getProducts)
-router.get('/:pid', getProductById)
-router.post('/add', saveProduct)
-router.put('/:pid', updateProduct)
-router.delete('/:pid', deleteProduct)
+router.get('/', passportCall('jwt'), getProducts);
+router.get('/:pid', getProductById);
+router.post('/add', saveProduct);
+router.put('/:pid', updateProduct);
+router.delete('/:pid', deleteProduct);
 
-export default router
+export default router;
