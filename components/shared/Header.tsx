fddd62c9e78e2996ad12ed7418ffaed0ed37c06e
Matias Barengo <77636500+MatiasBarengo@users.noreply.github.com>
2023-05-23 19:13:05 -0300

    Update Header.tsx

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,4 @@
 import Logo from '@/icons/Logo';
-import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import { Avatar } from '@material-tailwind/react';
 import Link from 'next/link';
 
@@ -43,12 +42,6 @@ const Header = () => {
             alt="candice wu"
             className="border border-blue-500 p-0.5"
             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
-          <ChevronDownIcon
-            strokeWidth={2.5}
-            // className={`h-3 w-3 transition-transform ${
-            //   isMenuOpen ? 'rotate-180' : ''
-            // }`}
           />
         </div>
       </div>
