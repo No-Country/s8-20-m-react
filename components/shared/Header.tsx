fc6644561f310be6e6c1af748225191d48c0b31b
Luis Simosa <simosa37@gmail.com>
2023-06-01 09:01:44 -0300

    feat: add profile link in header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -36,13 +36,15 @@ const Header = () => {
           <div className="relative flex w-full sm:w-7/12 md:w-5/12 px-4 flex-wrap items-stretch lg:ml-auto">
             <input type="text" className="" placeholder="Search" />
           </div>
-          <Avatar
-            variant="circular"
-            size="sm"
-            alt="candice wu"
-            className="border border-blue-500 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
+          <Link href="/profile">
+            <Avatar
+              variant="circular"
+              size="sm"
+              alt="candice wu"
+              className="border border-blue-500 p-0.5"
+              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+            />
+          </Link>
         </div>
       </div>
     </nav>
