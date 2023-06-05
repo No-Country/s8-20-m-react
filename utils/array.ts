6dc5709921c2dc697af176916bea4fb177e0a273
Linda Perea <linda.hp@hotmail.com>
2023-06-05 08:24:32 -0500

    feat: profileUsuario

---
+++ utils/array.ts
@@ -0,0 +1,72 @@
+    export const user = 
+    {
+      name: "Lucía",
+      city: "Buenos Aires, Argentina",
+      photo: "https://i1.rgstatic.net/ii/profile.image/1031678541717514-1622982740292_Q128/Olga-Torres-Vargas.jpg",
+    };
+  
+     export const imageDataBase = [
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 1",
+      subtitle: "Descripción 1"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 2",
+      subtitle: "Descripción 2"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 3",
+      subtitle: "Descripción 3"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 4",
+      subtitle: "Descripción 4"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 5",
+      subtitle: "Descripción 5"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 6",
+      subtitle: "Descripción 6"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 1",
+      subtitle: "Descripción 7"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 2",
+      subtitle: "Descripción 8"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 3",
+      subtitle: "Descripción 9"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 10",
+      subtitle: "Descripción 1"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 11",
+      subtitle: "Descripción 2"
+    },
+    {
+      src: "https://img.lovepik.com/element/45003/9415.png_860.png",
+      title: "Imagen 12",
+      subtitle: "Descripción 3"
+    },
+    
+  ];
+  
+  
