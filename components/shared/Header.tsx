417e560d65662c9873d849d1ccbe0a0f25108fa5
Maximiliano Damian Maiz <maximaizrw@gmail.com>
2023-06-06 13:47:43 -0300

    build: home

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,18 +1,18 @@
 import Logo from '@/icons/Logo';
+import Image from 'next/image';
 import Link from 'next/link';
-import Search from "./search"
-import Image from "next/image";
+import Search from './search';
 
 const Header = () => {
   // const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   // const closeMenu = () => setIsMenuOpen(false);
   //const handleSubmit = (query: string) => {
-    // aquí puedes hacer lo que desees con la query
-    //console.log(`Buscando: ${query}`)
+  // aquí puedes hacer lo que desees con la query
+  //console.log(`Buscando: ${query}`)
   //}
 
   return (
-    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-headerBg mb-3 shadow-[0px_6px_10px_4px_rgba(0,0,0,0.05),0px_2px_3px_rgba(0,0,0,0.04)]">
+    <nav className="relative z-50 flex flex-wrap items-center justify-between px-2 py-3 bg-headerBg mb-3 shadow-[0px_6px_10px_4px_rgba(0,0,0,0.05),0px_2px_3px_rgba(0,0,0,0.04)]">
       <div className="container px-4 flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
           <Link href={'/'}>
@@ -39,22 +39,20 @@ const Header = () => {
             </li>
           </ul>
           <div className="container mx-auto">
-      <Search  />
-        </div>
-        <Link
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-primary hover:opacity-75"
-                href={'/profile'}
-              >
-                 <Image
+            <Search />
+          </div>
+          <Link
+            className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-primary hover:opacity-75"
+            href={'/profile'}
+          >
+            <Image
               src="/Button.png"
               alt="perfil"
               width={140}
               height={140}
               quality={100}
-              
             />
-              </Link>
-
+          </Link>
         </div>
       </div>
     </nav>
