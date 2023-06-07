af0e56a2c5a18cdb89f163e0c550d86e8847b414
Luis Simosa <simosa37@gmail.com>
2023-06-07 09:01:25 -0300

    fix: svg props names

--- icons/Waves.tsx
+++ icons/Waves.tsx
@@ -10,10 +10,10 @@ const Waves = () => {
       <path
         d="M-97 2C-69.4403 2 -69.4403 40 -41.8806 40C-14.3209 40 -14.3209 2 13.2388 2C40.7985 2 40.7985 40 68.2936 40C95.8541 40 95.8541 2 123.348 2C150.909 2 150.909 40 178.403 40C205.963 40 205.963 2 233.522 2C261.082 2 261.082 40 288.642 40C316.2 40 316.2 2 343.761 2C371.321 2 371.321 40 398.881 40C426.44 40 426.44 2 454 2"
         stroke="#4F4F4F"
-        stroke-opacity="0.1"
-        stroke-width="3"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
+        strokeOpacity="0.1"
+        strokeWidth="3"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
       />
     </svg>
   );
